7fccec945ef7bb440c3d32ed15f6e7b72ecd4cab
Dm Ant <Dm@Ant.ru>
2021-11-04 19:16:38 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        QuadEquation quadEq;
+        quadEq = (double a, double b, double c) -> {
+            double d = b*b - 4*a*c; // дискриминант
+
+            if (d>=0) {
+                // Найти корни
+                SolutionQuadEquation roots = new SolutionQuadEquation();
+                roots.x1 = (-b-Math.sqrt(d))/(2*a);
+                roots.x2 = (-b+Math.sqrt(d))/(2*a);
+                return roots;
+            }
+            else
+                return null;
+        };
+        SolutionQuadEquation roots = quadEq.solve(1,-5,1);
+        if (roots==null)
+        System.out.println("Корней нет");
+    else
+        {
+            System.out.println("x1 = " + roots.x1);
+            System.out.println("x2 = " + roots.x2);
+        }
+    }
+}
+interface QuadEquation{
+    SolutionQuadEquation solve(double a, double b, double c);
+}
+class SolutionQuadEquation{
+    double x1;
+    double x2;
+}
